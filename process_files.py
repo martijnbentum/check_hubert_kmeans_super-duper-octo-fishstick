24167e0a45b3e6c1ec31a4b1a8cdb5fd8fbb09cc
martijnbentum <bentummartijn@gmail.com>
2025-06-20 17:17:34 +0200

    generate labels for audio files from cgn, mls and common voice

---
+++ process_files.py
@@ -0,0 +1,47 @@
+import kmeans
+import locations
+from progressbar import progressbar
+
+def process_cgn(n_files = 100, model = None, model_filename = None):
+    if model is None: model = kmeans.load_kmeans_model(model_filename)
+    fn = list(locations.cgn_news_sentences.glob('*.wav'))
+    for f in progressbar(fn[:n_files]):
+        output_filename = locations.labels / (f.stem + '.txt')
+        if output_filename.exists(): continue
+        labels = kmeans.audio_filename_to_labels(f, model)
+        kmeans.save_labels(labels, output_filename)
+
+
+
+def load_manifest():
+    with open(locations.pretrain_manifest) as fin:
+        manifest = fin.read().split('\n')
+    return manifest[1:-1]
+
+def get_mls_filenames(n_files = 100):
+    manifest = load_manifest()
+    output = [] 
+    directory = locations.pretrain_datasets
+    for line in manifest:
+        if not 'MLS' in line: continue
+        f = directory / line.split('\t')[0]
+        if not f.exists():
+            raise ValueError(f'could not find {f}')
+        output.append(str(f))
+        if len(output) > = n_files:break
+    return output
+
+    
+def get_cv_filenames(n_files = 100):
+    manifest = load_manifest()
+    output = [] 
+    directory = locations.pretrain_datasets
+    for line in manifest:
+        if not 'common_voice' in line: continue
+        f = directory / line.split('\t')[0]
+        if not f.exists():
+            raise ValueError(f'could not find {f}')
+        output.append(str(f))
+        if len(output) > = n_files:break
+    return output
+
