92b1fa2088e94fe1a95eb086b4aad447a19ea4d8
martijnbentum <bentummartijn@gmail.com>
2025-06-27 15:37:07 +0200

    added phoneme time alignment files

--- locations.py
+++ locations.py
@@ -5,7 +5,7 @@ kmeans_models = hubert_root / 'kmeans_models'
 kmeans_model_filename = kmeans_models / 'km_train_mfcc-model_100clusters'
 
 cluster_check = hubert_root / 'cluster_check'
-labels = cluster_check / 'labels'
+kmeans_labels = cluster_check / 'kmeans_labels'
 
 speech_training = Path('/projects/0/prjs0893/speech-training')
 
@@ -19,3 +19,9 @@ common_voice = pretrain_datasets / 'cv_wav'
 mls = pretrain_datasets / 'MLS_dutch_wav'
 
 pretrain_manifest = data / 'manifests/dataset-960h-nl/train.tsv'
+
+
+metadata = Path('/projects/0/prjs0893/speech-training/metadata')
+cgn_news_phonemes = metadata / 'news_books_phonemes_zs.tsv'
+mls_phonemes = metadata / 'dutch_mls_phonemes_zs.tsv'
+cv_phonemes = metadata / 'dutch_cv_phonemes_zs.tsv'
