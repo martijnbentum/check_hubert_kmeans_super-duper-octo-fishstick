ecd0f0ded525a01b7bdfd31963d786a0b198323e
martijnbentum <bentummartijn@gmail.com>
2025-07-09 15:41:38 +0200

    renamed file to m1 checking stage 1 of the model

--- process_files_m1.py
+++ process_files_m1.py
@@ -4,30 +4,33 @@ from pathlib import Path
 from progressbar import progressbar
 
 def process_cgn(n_files = 1000, model = None, model_filename = None):
-    if model is None: model = kmeans.load_kmeans_model(model_filename)
+    if model is None: 
+        model = kmeans.load_kmeans_model(model_filename)
     fn = list(locations.cgn_news_sentences.glob('*.wav'))
     for f in progressbar(fn[:n_files]):
-        output_filename = locations.kmeans_labels / (f.stem + '.txt')
+        output_filename = locations.cl_kmeans_labels_mfcc / (f.stem + '.txt')
         if output_filename.exists(): continue
         labels = kmeans.audio_filename_to_labels(f, model)
         kmeans.save_labels(labels, output_filename)
 
 def process_mls(n_files = 300, model = None, model_filename = None):
-    if model is None: model = kmeans.load_kmeans_model(model_filename)
+    if model is None: 
+        model = kmeans.load_kmeans_model(model_filename)
     fn = locations.get_mls_filenames(n_files)
     for f in progressbar(fn[:n_files]):
         f = Path(f)
-        output_filename = locations.kmeans_labels / (f.stem + '.txt')
+        output_filename = locations.cl_kmeans_labels_mfcc / (f.stem + '.txt')
         if output_filename.exists(): continue
         labels = kmeans.audio_filename_to_labels(f, model)
         kmeans.save_labels(labels, output_filename)
 
 def process_cv(n_files = 1000, model = None, model_filename = None):
-    if model is None: model = kmeans.load_kmeans_model(model_filename)
+    if model is None: 
+        model = kmeans.load_kmeans_model(model_filename)
     fn = locations.get_cv_filenames(n_files)
     for f in progressbar(fn[:n_files]):
         f = Path(f)
-        output_filename = locations.kmeans_labels / (f.stem + '.txt')
+        output_filename = locations.cl_kmeans_labels_mfcc / (f.stem + '.txt')
         if output_filename.exists(): continue
         labels = kmeans.audio_filename_to_labels(f, model)
         kmeans.save_labels(labels, output_filename)
